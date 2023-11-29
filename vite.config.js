4a258e9660b3e889b7dad0770cb3f1ea45357733
giuseppevignanello <peppe.vignanello@gmail.com>
2023-11-29 12:44:16 +0100

    fix npm run dev bug

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-const path = require('path') // <-- require path from node
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             // edit the first value of the array input to point to our new sass files and folder.
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
         }),
     ],
     // Add resolve object and aliases
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~resources': '/resources/'
-        }
-    }
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+            "~resources": "/resources/",
+        },
+    },
 });
