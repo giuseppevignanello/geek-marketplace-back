534535f65ad45411726455a62ac6d6f5c3618d11
giuseppevignanello <peppe.vignanello@gmail.com>
2023-11-30 17:15:49 +0100

    add Shop Index

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,6 +13,10 @@ class ShopController extends Controller
      */
     public function index()
     {
+
+        //add auth
+        $shop = Shop::first();
+        return view('shop.index', compact('shop'));
     }
 
     /**
@@ -36,7 +40,6 @@ class ShopController extends Controller
      */
     public function show(Shop $shop)
     {
-        //
     }
 
     /**
